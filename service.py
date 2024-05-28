5140fdb98d1539c0d498b5c688c54fd173afffb2
Ar1ssian <alxkomarov@mail.ru>
2024-05-28 14:52:11 +0300

    Update service.py

--- service.py
+++ service.py
@@ -36,6 +36,5 @@ def order():
         print('Нет данных')
         return render_template('order.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
